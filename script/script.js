88f851fb7b137070500066d3d77c1397b48b2b57
Vladislav-Oleksienko <splinfao1@gmail.com>
2022-09-20 13:32:28 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const container = document.querySelector(".tbody-table");
+
+const response = fetch("https://jsonplaceholder.typicode.com/photos?_limit=20")
+  .then((response) => response.json())
+  .then((res) => res.map((post) => getPosts(post)));
+
+
+const getPosts = (post) => {
+  container.innerHTML += `
+  <tr>
+    <td class="table-id">${post.id}</td>
+    <td>${post.title}</td>
+    <td>
+      <img src=${post.url} alt="img" class="main-imgs">
+    </td>
+  </tr>`;
+};
